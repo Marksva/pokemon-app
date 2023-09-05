20ef02e0ee3c57ce00e9c9645f3dee59c6be7a64
Marksva <mark.sva@hotmail.com>
2023-09-05 20:42:01 -0300

    app.js

---
+++ api/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = require('./routes/routes.js');
+const cors = require('cors');
+const config = require('../config.js');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(router);
+
+app.listen(config.env.PORT, () => {
+    console.log('server runing');
+});
